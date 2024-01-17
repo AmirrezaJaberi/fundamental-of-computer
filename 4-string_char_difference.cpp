ca733662b1c4d6c1d41cc8b0bff5472a83d90b86
Amirreza Jaberi <amirrezajabery112@gmail.com>
2024-01-17 19:02:30 +0330

    Added basic code for valuable difference

---
+++ 4-string_char_difference.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+#include <string>
+
+int main()
+{
+    // This is a string example using "double quotes" (").
+
+    std::string myString = "Hello, World!";
+
+    // This is a character example using 'single quotes' ('').
+
+    char myChar = 'A';
+
+    // Print the values
+    std::cout << "String: " << myString << std::endl;
+    std::cout << "Char: " << myChar << std::endl;
+
+    return 0;
+}
