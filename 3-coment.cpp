4e3364616df4686963a95345fc67b6ad3a2be074
Amirreza Jaberi <amirrezajabery112@gmail.com>
2024-01-17 18:57:25 +0330

    Added comment basic code

---
+++ 3-coment.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+
+int main()
+{
+
+    // This is a single-line comment
+
+    /*
+    This is a multi-line comment.
+    It can span multiple lines.
+    */
+
+    return 0;
+}
