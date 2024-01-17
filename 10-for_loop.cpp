617b4ced5bc2e3e17baaedfb184456b5f562e340
Amirreza Jaberi <amirrezajabery112@gmail.com>
2024-01-17 19:56:49 +0330

    Added for loop example code

---
+++ 10-for_loop.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+
+int main()
+{
+    // For loop to print numbers from 1 to 5
+    for (int i = 1; i <= 5; i++)
+    {
+        std::cout << i << " ";
+    }
+
+    std::cout << std::endl;
+
+    return 0;
+}
