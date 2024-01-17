92e0a54d0c5bc073aa7508a23b855dda3ea1d900
Amirreza Jaberi <amirrezajabery112@gmail.com>
2024-01-17 19:47:52 +0330

    Added example code for while loop

---
+++ 9-while_loop.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+
+int main()
+{
+    int counter = 1;
+
+    // Using a while loop to print numbers from 1 to 5
+    while (counter <= 5)
+    {
+        std::cout << counter << " ";
+        counter++;
+    }
+
+    std::cout << std::endl;
+
+    return 0;
+}
