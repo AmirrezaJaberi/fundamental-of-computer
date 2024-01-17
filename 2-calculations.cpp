f8d73d26a3fefe06077f7acf065cfab69363636f
Amirreza Jaberi <amirrezajabery112@gmail.com>
2024-01-17 18:55:30 +0330

    Added basic code of calculations

---
+++ 2-calculations.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+
+int main()
+{
+    // Sum
+    int sum = 5 + 3;
+
+    // Difference
+    int difference = 7 - 4;
+
+    // Product
+    int product = 6 * 2;
+
+    // Quotient
+    float quotient = 8.0 / 2;
+
+    // Remainder
+    int remainder = 9 % 4;
+
+    // Print the results
+    std::cout << "Sum: " << sum << std::endl;
+    std::cout << "Difference: " << difference << std::endl;
+    std::cout << "Product: " << product << std::endl;
+    std::cout << "Quotient: " << quotient << std::endl;
+    std::cout << "Remainder: " << remainder << std::endl;
+
+    return 0;
+}
